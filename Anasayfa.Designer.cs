d0d9cc262427727a680a3e329dd79f2a65dda683
dogukankosan <77827358+dogukankosan@users.noreply.github.com>
2021-11-16 12:49:13 +0300

    DatabaseOffline
    ÇOK VERİ TABANI OLAN PROGRAMLARDA KULLANILMAYAN VERİ TABANLARINI OFFLINE YAPIP GEREKTİĞİNDE ONLINE YAPACAKTIR

---
+++ Anasayfa.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace VeriTabanıOffline
+{
+    partial class Anasayfa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Anasayfa));
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.repositoryItemCheckEdit1 = new DevExpress.XtraEditors.Repository.RepositoryItemCheckEdit();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.tümünüSeçToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tümünüKapatToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemCheckEdit1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.ContextMenuStrip = this.contextMenuStrip1;
+            this.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridControl1.EmbeddedNavigator.Margin = new System.Windows.Forms.Padding(38);
+            this.gridControl1.Location = new System.Drawing.Point(0, 0);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Margin = new System.Windows.Forms.Padding(38);
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.RepositoryItems.AddRange(new DevExpress.XtraEditors.Repository.RepositoryItem[] {
+            this.repositoryItemCheckEdit1});
+            this.gridControl1.Size = new System.Drawing.Size(763, 696);
+            this.gridControl1.TabIndex = 1;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // gridView1
+            // 
+            this.gridView1.Appearance.Empty.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.gridView1.Appearance.Empty.BackColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.gridView1.Appearance.Empty.Options.UseBackColor = true;
+            this.gridView1.Appearance.FocusedCell.BackColor = System.Drawing.Color.Teal;
+            this.gridView1.Appearance.FocusedCell.Options.UseBackColor = true;
+            this.gridView1.Appearance.FocusedRow.BackColor = System.Drawing.Color.Teal;
+            this.gridView1.Appearance.FocusedRow.BackColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.gridView1.Appearance.FocusedRow.Options.UseBackColor = true;
+            this.gridView1.Appearance.HeaderPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.gridView1.Appearance.HeaderPanel.BackColor2 = System.Drawing.Color.Gray;
+            this.gridView1.Appearance.HeaderPanel.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold);
+            this.gridView1.Appearance.HeaderPanel.ForeColor = System.Drawing.Color.White;
+            this.gridView1.Appearance.HeaderPanel.Options.UseBackColor = true;
+            this.gridView1.Appearance.HeaderPanel.Options.UseFont = true;
+            this.gridView1.Appearance.HeaderPanel.Options.UseForeColor = true;
+            this.gridView1.Appearance.Row.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.gridView1.Appearance.Row.BackColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.gridView1.Appearance.Row.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.gridView1.Appearance.Row.ForeColor = System.Drawing.Color.Black;
+            this.gridView1.Appearance.Row.Options.UseBackColor = true;
+            this.gridView1.Appearance.Row.Options.UseFont = true;
+            this.gridView1.Appearance.Row.Options.UseForeColor = true;
+            this.gridView1.Appearance.SelectedRow.BackColor = System.Drawing.Color.Teal;
+            this.gridView1.Appearance.SelectedRow.BackColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.gridView1.Appearance.SelectedRow.Options.UseBackColor = true;
+            this.gridView1.DetailHeight = 546;
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            this.gridView1.OptionsBehavior.ReadOnly = true;
+            this.gridView1.OptionsFind.AlwaysVisible = true;
+            this.gridView1.OptionsView.ShowGroupPanel = false;
+            this.gridView1.RowClick += new DevExpress.XtraGrid.Views.Grid.RowClickEventHandler(this.gridView1_RowClick);
+            // 
+            // repositoryItemCheckEdit1
+            // 
+            this.repositoryItemCheckEdit1.AutoHeight = false;
+            this.repositoryItemCheckEdit1.Name = "repositoryItemCheckEdit1";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tümünüSeçToolStripMenuItem,
+            this.tümünüKapatToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(211, 80);
+            // 
+            // tümünüSeçToolStripMenuItem
+            // 
+            this.tümünüSeçToolStripMenuItem.Name = "tümünüSeçToolStripMenuItem";
+            this.tümünüSeçToolStripMenuItem.Size = new System.Drawing.Size(210, 24);
+            this.tümünüSeçToolStripMenuItem.Text = "Tümünü Aç";
+            this.tümünüSeçToolStripMenuItem.Click += new System.EventHandler(this.tümünüSeçToolStripMenuItem_Click);
+            // 
+            // tümünüKapatToolStripMenuItem
+            // 
+            this.tümünüKapatToolStripMenuItem.Name = "tümünüKapatToolStripMenuItem";
+            this.tümünüKapatToolStripMenuItem.Size = new System.Drawing.Size(210, 24);
+            this.tümünüKapatToolStripMenuItem.Text = "Tümünü Kapat";
+            this.tümünüKapatToolStripMenuItem.Click += new System.EventHandler(this.tümünüKapatToolStripMenuItem_Click);
+            // 
+            // Anasayfa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(763, 696);
+            this.Controls.Add(this.gridControl1);
+            this.IconOptions.LargeImage = ((System.Drawing.Image)(resources.GetObject("Anasayfa.IconOptions.LargeImage")));
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "Anasayfa";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ANA SAYFA";
+            this.Load += new System.EventHandler(this.Anasayfa_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Anasayfa_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemCheckEdit1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private DevExpress.XtraEditors.Repository.RepositoryItemCheckEdit repositoryItemCheckEdit1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem tümünüSeçToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tümünüKapatToolStripMenuItem;
+    }
+}
---
+++ Anasayfa.cs
@@ -0,0 +1,99 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace VeriTabanıOffline
+{
+    public partial class Anasayfa : DevExpress.XtraEditors.XtraForm
+    {
+        SqlConnection myconnect = new SqlConnection(System.IO.File.ReadAllText("Database.txt"));
+        void DatabaseList(string command)
+        {
+            SqlDataAdapter da = new SqlDataAdapter(command, myconnect);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            gridControl1.DataSource = ds.Tables[0];
+        }
+        public Anasayfa()
+        {
+            InitializeComponent();
+        }
+        private void Anasayfa_Load(object sender, EventArgs e)
+        {
+            DatabaseList("select name as 'VERİ TABANI ADI',SIRKET.SIRKISAADI as 'SİRKET ADI',state_desc as 'DURUMU' from sys.databases inner join SIRKET on 'ETA_'+SIRKET.SIRKOD+'_'+Convert(varchar,SIRKET.SIRDONEM)=name");
+        }
+        private void Anasayfa_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                DialogResult cv = new DialogResult();
+                cv = XtraMessageBox.Show("ÇIKIŞ YAPMAK İSTEDİĞİNİZDEN EMİN MİSİNİZ ?", "ÇIKIŞ İŞLEMİ", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (cv == DialogResult.Yes)
+                    Application.Exit();
+            }
+        }
+        void AllStatusChanged(string status)
+        {
+            for (Int16 i = 0; i < gridView1.RowCount; i++)
+            {
+                try
+                {
+                    myconnect.Open();
+                    SqlCommand command = new SqlCommand("ALTER DATABASE " + gridView1.GetRowCellDisplayText(i, "VERİ TABANI ADI") + " SET " + status + " WITH ROLLBACK IMMEDIATE", myconnect);
+                    command.ExecuteNonQuery();
+                    myconnect.Close();
+                    DatabaseList("select name as 'VERİ TABANI ADI',SIRKET.SIRKISAADI as 'SİRKET ADI',state_desc as 'DURUMU' from sys.databases inner join SIRKET on 'ETA_'+SIRKET.SIRKOD+'_'+Convert(varchar,SIRKET.SIRDONEM)=name");
+                }
+                catch (Exception a)
+                {
+                    XtraMessageBox.Show(a.Message, "HATALI VERİ TABANI İŞLEMİ !", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    myconnect.Close();
+                }
+            }
+        }
+        void StatusChanged(string databasename, string status)
+        {
+            try
+            {
+                myconnect.Open();
+                if (status.ToString() == "ONLINE")
+                {
+                    SqlCommand command = new SqlCommand("ALTER DATABASE " + databasename.ToString() + " SET OFFLINE WITH ROLLBACK IMMEDIATE", myconnect);
+                    command.ExecuteNonQuery();
+                    myconnect.Close();
+                }
+                else
+                {
+                    SqlCommand command = new SqlCommand("ALTER DATABASE " + databasename.ToString() + "  SET ONLINE WITH ROLLBACK IMMEDIATE", myconnect);
+                    command.ExecuteNonQuery();
+                    myconnect.Close();
+                }
+                DatabaseList("select name as 'VERİ TABANI ADI',SIRKET.SIRKISAADI as 'SİRKET ADI',state_desc as 'DURUMU' from sys.databases inner join SIRKET on 'ETA_'+SIRKET.SIRKOD+'_'+Convert(varchar,SIRKET.SIRDONEM)=name");
+            }
+            catch (Exception a)
+            {
+                XtraMessageBox.Show(a.Message, "HATALI VERİ TABANI İŞLEMİ !", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                myconnect.Close();
+            }
+        }
+        private void gridView1_RowClick(object sender, DevExpress.XtraGrid.Views.Grid.RowClickEventArgs e)
+        {
+            StatusChanged(gridView1.GetFocusedRowCellValue("VERİ TABANI ADI").ToString(), gridView1.GetFocusedRowCellValue("DURUMU").ToString());
+        }
+        private void tümünüSeçToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AllStatusChanged("ONLINE");
+        }
+        private void tümünüKapatToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AllStatusChanged("OFFLINE");
+        }
+    }
+}
